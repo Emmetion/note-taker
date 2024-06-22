c23b223c81d65fbcc3b9e1762af37716d0affe35
Emmetion <emmetwormbox6@gmail.com>
2024-06-21 23:01:53 -0400

    notepreview edits

--- client/src/pages/MyNotes/NotePreview.jsx
+++ client/src/pages/MyNotes/NotePreview.jsx
@@ -7,6 +7,7 @@ import { useNavigate } from "react-router-dom";
 export default function NotePreview( {note} ) {
 
     const [editColor, setEditColor] = useState('black')
+
     const nav = useNavigate();
 
     const editNoteButton = () => {
@@ -15,7 +16,7 @@ export default function NotePreview( {note} ) {
 
 
     return (
-        <div className="w-[300px] h-[250px] rounded border-4 border-gray-300 bg-gray-300 flex flex-col shadow-md hover:scale-105 hover:shadow-lg hover:shadow-gray-500 duration-100 cursor-pointer"
+        <div className="w-[300px] h-[250px] rounded-lg border-[1px] shadow-lg border-black flex flex-col shadow-md hover:scale-105 hover:shadow-lg hover:shadow-gray-500 duration-100 cursor-pointer"
             onClick={() => editNoteButton()}>
             {/* Title of Note */}
             <div className="w-full text-black ">
@@ -24,9 +25,9 @@ export default function NotePreview( {note} ) {
             {/* Type of Note */}
             <div className="pl-4 mr-auto text-black flex flex-row">
                 <Typography variant="h5" className="my-auto mr-2">Tags:{' '}</Typography>
-                {
+                {/* {
                     note.noteTags.map((tag) => <Chip value={tag} className="my-auto mr-2"></Chip>)
-                }
+                } */}
                 <Chip value={note.noteType} className="my-auto"></Chip>
             </div>
 
